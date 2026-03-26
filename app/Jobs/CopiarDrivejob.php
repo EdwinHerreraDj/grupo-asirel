60b15147a2606640e39f81b3e483212b604ac13d
EdwinHerreraDj <edwin2000gonzalezzz@gmail.com>
2026-03-26 11:19:28 +0100

    fix: corrige nombres de archivos y carpetas para PSR-4

--- app/Jobs/CopiarDrivejob.php
+++ app/Jobs/CopiarDrivejob.php
@@ -12,7 +12,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 
-class CopiarDriveJob implements ShouldQueue
+class CopiarDrivejob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
